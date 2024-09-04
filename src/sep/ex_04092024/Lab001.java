b8b52908b81960fffd2a01bc43b9fff8c32c7394
Anand <anandpgems@gmail.com>
2024-09-04 08:23:27 +0530

    Initial commit

---
+++ src/sep/ex_04092024/Lab001.java
@@ -0,0 +1,16 @@
+package sep;
+
+public class Lab001 {
+    public static void main(String[] args) {
+        int age = 99;
+        Long phoneno = 8289900174l;
+        System.out.printf("Hello My age is %d \n",age);
+        System.out.println("Hello My age is "+age);
+        System.out.println(2+2);
+        System.out.println(2-2);
+        System.out.println(2*2);
+        System.out.println(2/2);
+    }
+
+
+}
