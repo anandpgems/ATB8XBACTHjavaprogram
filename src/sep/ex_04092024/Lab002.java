5d9dac459ae39fd7888645547660043bd8d30952
Anand <anandpgems@gmail.com>
2024-09-06 18:59:24 +0530

    added Lab003.java

--- src/sep/ex_04092024/Lab002.java
+++ src/sep/ex_04092024/Lab002.java
@@ -1,4 +1,13 @@
 package sep.ex_04092024;
 
 public class Lab002 {
+public static void main(String[] args) {
+        String name = "Anand";
+    System.out.println(name);
+    System.out.printf("Your name is -> %s",name);
+
+
+
+
+}
 }
--- src/sep/ex_04092024/Lab003.java
+++ src/sep/ex_04092024/Lab003.java
@@ -1,4 +1,10 @@
 package sep.ex_04092024;
 
 public class Lab003 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Java");
+    }
+
 }
